6705b97008358bd36478d45810859f4194383eae
leoyyer <249049410@qq.com>
2024-08-08 10:40:47 +0800

    Update setup.py
    add  pandas==2.2.2

--- setup.py
+++ setup.py
@@ -55,7 +55,8 @@ setup(
         "xlrd==1.2.0",
         "xlutils==2.0.0",
         "xlwt==1.3.0",
-        "zipp==3.15.0"
+        "zipp==3.15.0",
+        "pandas==2.2.2"
 
     ],
     entry_points={
