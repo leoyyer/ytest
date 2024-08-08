7f89cd20b25b995a385114fb6c2ef19701fe9424
pengda01 <pengda01@mingyuanyun.com>
2024-08-08 10:08:58 +0800

    Revert "修改目录结构"
    This reverts commit 09b7ecadb5f252015ba651af616949c975d517c7.

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     name="ytest",
     version="0.1.0",
     author="leo",
+    author_email="your.email@example.com",
     description="auto api test",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -17,7 +18,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires='>=3.6',
     install_requires=[
         "allure-pytest==2.13.5",
         "allure-python-commons==2.13.5",
@@ -54,11 +55,12 @@ setup(
         "xlrd==1.2.0",
         "xlutils==2.0.0",
         "xlwt==1.3.0",
-        "zipp==3.15.0",
+        "zipp==3.15.0"
+
     ],
     entry_points={
-        "console_scripts": [
-            "ytest=ytest.__main__:main",
+        'console_scripts': [
+            'ytest=ytest.__main__:main',
         ]
-    },
+    }
 )
